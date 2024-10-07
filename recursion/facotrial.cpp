4f0e9b57135692c701dce500fae24899f7d19bfe
Siddhartha Basnet <siddbasnet2002@gmail.com>
2024-10-07 22:00:18 +0545

    recursion question:factorial

---
+++ recursion/facotrial.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// functional recursion
+int fact(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact(n - 1);
+    }
+}
+
+int main()
+{
+    int n = 5;
+    cout << fact(n) << endl;
+    return 0;
+}
