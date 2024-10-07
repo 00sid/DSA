2ee072c9f1dddf1a91efb5092df470c6f39e7444
Siddhartha Basnet <siddbasnet2002@gmail.com>
2024-10-07 22:00:43 +0545

    recursion question:factorial with comment

--- recursion/facotrial.cpp
+++ recursion/facotrial.cpp
@@ -4,7 +4,12 @@ using namespace std;
 // functional recursion
 int fact(int n)
 {
-    if (n == 0)
+    if (n == 0) // first 3*f(2)
+                // second 2*f(1)
+                // third 1*f(0)
+                // f(0) returns 1;
+                // f(1) returns 1*1
+                // similarly f(3) returns 3*2*1*1
     {
         return 1;
     }
@@ -16,7 +21,7 @@ int fact(int n)
 
 int main()
 {
-    int n = 5;
+    int n = 3;
     cout << fact(n) << endl;
     return 0;
 }
